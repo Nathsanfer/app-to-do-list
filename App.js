477ac84c002d9fac765787e3d3499ef0bc3a0051
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-08-31 21:35:18 -0300

    feat: create Header

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Image } from 'react-native';
+import { useFonts } from "expo-font";
 
 export default function App() {
+
+  {/* Fonts */}
+  const [fontsLoaded] = useFonts({
+    Garamond: require("./assets/fonts/EBGaramond-Regular.ttf"),
+    GaramondBold: require("./assets/fonts/EBGaramond-Bold.ttf"),
+    Emblema: require("./assets/fonts/EmblemaOne-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      <Text>Lista de Tarefas</Text>
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+
+      {/* Header */}
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.saudacao}>Olá Nathalia!</Text>
+          <Text style={styles.subtitulo}>Acompanhe sua agenda de tarefas aqui!</Text>
+          <View style={styles.divisor} />
+        </View>
+        <Image style={styles.imagem}
+          source={require("./assets/image.png")}
+        />
+      </View>
+
     </View>
   );
 }
@@ -12,7 +36,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20
   },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  saudacao: {
+    fontSize: 28,
+    fontFamily: "GaramondBold",
+    marginBottom: 6
+  },
+  subtitulo: {
+    fontSize: 14,
+    fontFamily: "Garamond",
+    color: '#555'
+  },
+  divisor: {
+    height: 1,
+    backgroundColor: '#eee',
+    marginVertical: 12,
+    width: '80%'
+  },
+  imagem: {
+    width: 150,
+    height: 200,
+    resizeMode: 'contain'
+  }
 });
